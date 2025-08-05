07b2d02271bebaa9c93bb8c17fb09562e3a6c79f
JungWoo55 <dave5251099@gmail.com>
2025-08-05 15:23:58 +0900

    08051521

---
+++ src/main/java/pack/Spring03Application.java
@@ -0,0 +1,19 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Spring03Application {
+	@GetMapping("/")//기본 경로
+	public String open() {
+		return "Spring03 Start : 08051450";
+	}
+	public static void main(String[] args) {
+		System.out.println("Spring03 Start");
+		SpringApplication.run(Spring03Application.class, args);
+	}
+}
